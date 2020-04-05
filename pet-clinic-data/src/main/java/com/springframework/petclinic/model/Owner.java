147839850ff75a32b7f23cad20b9c1ef00d676d2
marquislek <marquislek@gmail.com>
2020-04-05 20:54:45 +0800

    added properties

--- pet-clinic-data/src/main/java/com/springframework/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/model/Owner.java
@@ -3,7 +3,35 @@ package com.springframework.petclinic.model;
 import java.util.Set;
 
 public class Owner extends Person {
+
+    private String address;
+    private String city;
+    private String Telephone;
     private Set<Pet> pets;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return Telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        Telephone = telephone;
+    }
 
     public Set<Pet> getPets() {
         return pets;
