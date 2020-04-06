f3bf3caf63b5f6ec2c9975928c302b5fdde6142a
marquislek <marquislek@gmail.com>
2020-04-06 09:14:17 +0800

    Added specialties map impl

--- pet-clinic-data/src/main/java/com/springframework/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/model/Vet.java
@@ -5,13 +5,13 @@ import java.util.Set;
 
 public class Vet extends Person {
 
-    private Set<Specialty> specialties = new HashSet<>();
+    private Set<Speciality> specialties = new HashSet<>();
 
-    public Set<Specialty> getSpecialities(){
+    public Set<Speciality> getSpecialities(){
         return specialties;
     }
 
-    public void setSpecialties(Set<Specialty> specialties) {
+    public void setSpecialties(Set<Speciality> specialties) {
         this.specialties = specialties;
     }
 }
---
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/services/map/SpecialitiesMapService.java
@@ -0,0 +1,36 @@
+package com.springframework.petclinic.services.map;
+
+import com.springframework.petclinic.model.Speciality;
+import com.springframework.petclinic.services.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialitiesMapService extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findByID(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteByID(Long id) {
+        super.deleteById(id);
+    }
+}
